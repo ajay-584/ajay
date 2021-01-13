0cb08018ad81e6d2f58dee0695f8efc5feb2353e
Ajay Kumar <sliet584@gmail.com>
2021-01-13 21:07:06 +0530

    ..

---
+++ blog/script.js
@@ -0,0 +1,8 @@
+    // =============javascript code=========================
+    function openNav() {
+      document.getElementById("mySidenav").style.width = "250px";
+    }
+    
+    function closeNav() {
+      document.getElementById("mySidenav").style.width = "0";
+    }
